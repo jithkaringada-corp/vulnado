99700eca59b0510fdb9e4cc1c4c1c3d7f653c6ad
Jith Karingada <jith.karingada@sap.com>
2022-04-14 11:12:35 -0400

    sanitize URL

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -4,6 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
@@ -14,10 +16,20 @@ import java.io.IOException;
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
+    boolean validUrl = url.matches("^(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$");
+    if(validUrl) {
+      return LinkLister.getLinks(url);
+    }else {
+      return new LinkedList<>();
+    }
   }
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
+    boolean validUrl = url.matches("^(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$");
+    if(validUrl) {
+      return LinkLister.getLinksV2(url);
+    }else {
+      return new LinkedList<>();
+    }
   }
 }
